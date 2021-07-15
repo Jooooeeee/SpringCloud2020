ad9cb0f6e958d4c9cbae8d6e701fa3f3224aac95
Jooooeeee <1454239240@qq.com>
2021-07-15 23:10:46 +0800

    配置中心实现

--- cloud-config-3366/src/main/java/config/controller/ConfigClientController.java
+++ cloud-config-3366/src/main/java/config/controller/ConfigClientController.java
@@ -1,4 +1,4 @@
-package com.joe.config.controller;
+package config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
